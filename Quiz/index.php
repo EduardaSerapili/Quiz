4474f7fbebf1d42bde338acefd415e5a12c2bbf0
EduardaSerapili <eduardaserapili@gmail.com>
2024-04-22 19:39:08 -0300

    Subindo no git

---
+++ Quiz/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quiz || Feira-Técnologica</title>
+    <!--================CSS EXTERNO================-->
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+
+    <section class="start" id="iniciar">
+        <h1>Seja Bem-Vindo ao Quiz!</h1>
+        <br>
+        <p class="quiz">Este quiz foi elaborado para desafiar seus conhecimentos gerais na área da tecnologia.</p>
+        <p class="desafio">Você está preparado para este desafio?</p>
+
+        <button id="start" onclick="iniciar()">START</button>
+    </section>
+    <div id="linhatempo">
+    </div>
+
+    <section class="corpo" id="corpo">
+        
+        <article class="quiz">
+            <div id="linhatempo">
+            </div>
+
+            <h2 id="questoes">Selecione o nível de dificuldade:</h2>
+            <div id="alternativasButton">
+                <button class="btn" onclick="startEasy()">Fácil</button>
+                <button class="btn" onclick="startMed()">Médio</button>
+                <button class="btn" onclick="startHard()">Difícil</button>
+            </div>
+            <button id="proximo">Próximo</button>  
+            <p id="frase"></p>    
+        </article>
+    </section>
+    <footer id="creditos">
+        <section class="grid">
+            <div class="dev">
+                <h5 class="titulof">Desenvolvido por:</h5>
+                <ul class="menu">
+                    <li>Marcus Vannucchi</li>
+                    <li>Maria Eduarda</li>
+                    <li>Otavio Fernandes</li>
+                    <li>Rodrigo Santos</li>
+                    <li>Tarcísio Neves</li>
+                </ul>
+            </div>
+            <div class="sobrenos">
+                <h5 class="titulof">Sobre Quiz:</h5>
+                    <p>Este projeto foi desenvolvido a fim de testar o conhecimento tecnológico de seus participantes. Com o intuito de ensinar sobre a área de uma maneira dinâmica e descontraída.</p>
+
+            </div>
+            <div class="tec">
+                <h5 class="titulof">Tecnologias usadas:</h5>
+                <ul class="menu">
+                    <li>HTML5</li>
+                    <li>CSS3</li>
+                    <li>Java Script</li>
+                </ul>
+            </div>
+        </section>
+        <div class="etec">
+            <p>Etec Zona Leste | Centro Paula Souza &copy; 2023 </p>
+        </div>
+    </footer>
+
+    <script src="index.js"></script>  
+</body>
+</html>
